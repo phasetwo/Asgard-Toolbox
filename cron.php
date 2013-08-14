4c1bdbaaf3d604f7647999b94fb2176f97c69441
David Mendek <dmendek@gmail.com>
2013-08-14 18:19:09 +0200

    Update script for ranks.

--- cron.php
+++ cron.php
@@ -3,7 +3,6 @@ require("./getRanking.php");
 
 if($_GET['security'] = "4028a0e356acc947fcd2bfbf00cef11e128d484a") {
 	$rank = new rankHandler("localhost", "bf", "bf", "bf");
-	
 	$rank->readRanks();
 	$rank->parseRanks();
 	$rank->saveToDB();
